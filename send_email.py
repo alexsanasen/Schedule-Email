24fcd313ff9165879038fa5fb544312eba3e285f
Alex Sanasen <alex.sanasen@gmail.com>
2023-09-05 22:38:16 -0700

    nc

--- send_email.py
+++ send_email.py
@@ -4,6 +4,7 @@ import os
 
 port = 465
 smtp_server = "smtp.gmail.com"
+SENDER = "Do not reply"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
 message = """\
@@ -15,4 +16,4 @@ This is your daily email report.
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME,PASSWORD)
-    server.sendmail("Do not reply",USERNAME,message)
+    server.sendmail(SENDER,USERNAME,message)
